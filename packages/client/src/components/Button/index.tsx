2bce5481bfdeb698c4e1a31fe1aab872fd51d62f
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2023-04-25 04:10:40 +0500

    Стиливые константы, палитры и шрифт

--- packages/client/src/components/Button/index.tsx
+++ packages/client/src/components/Button/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './Button.pcss'
 
-type StyleTypes = 'primary' | 'secondary' | 'tertiary' | 'disabled' | 'error' // TODO можно перенести в общий файл с типами
-
 interface ButtonProps
   extends React.DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -18,14 +16,6 @@ interface ButtonProps
   onClick?: (event: React.MouseEvent) => void
 }
 
-const ButtonPallete = {
-  primary: { main: '#FB923C', border: '#FB923C', color: '#FFF' },
-  secondary: { main: 'transparent', border: '#FB923C', color: '#FB923C' },
-  tertiary: { main: 'transparent', border: '#D4D4D8', color: '#A1A1AA' },
-  disabled: { main: '#D9D9D9', border: '#D9D9D9', color: '#A1A1AA' },
-  error: { main: '#8F0F2B', border: '#8F0F2B', color: '#FFF' }
-}
-
 const Button: React.FC<ButtonProps> = ({
   classNames = '',
   styleType = 'primary',
@@ -38,14 +28,9 @@ const Button: React.FC<ButtonProps> = ({
   margin,
   ...rest
 }) => {
-  const selectedStyle = disabled
-    ? ButtonPallete.disabled
-    : ButtonPallete[styleType]
+  const currentStyle = disabled ? 'disabled' : styleType
 
   const buttonStyle = {
-    backgroundColor: selectedStyle.main,
-    borderColor: selectedStyle.border,
-    color: selectedStyle.color,
     padding,
     margin,
     height,
@@ -54,7 +39,7 @@ const Button: React.FC<ButtonProps> = ({
 
   return (
     <button
-      className={'button ' + classNames}
+      className={`button  ${classNames} ${currentStyle}`}
       disabled
       style={buttonStyle}
       onClick={onClick}
