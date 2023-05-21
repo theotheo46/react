6b2565896a1d2171cdb694b50f66c4f63e8e8884
an3wers <an3wer@yandex.ru>
2023-05-21 09:27:50 +0500

    formatt

--- packages/client/src/components/Form/index.test.tsx
+++ packages/client/src/components/Form/index.test.tsx
@@ -23,40 +23,39 @@ const reactForm: React.ReactElement = (
 )
 
 describe('Form component', () => {
+  test('elements check', () => {
+    const { container } = render(reactForm)
+    const loginInput = container.querySelector('#login')
+    const passwordInput = container.querySelector('#password')
+    expect(loginInput).toBeTruthy()
+    expect(passwordInput).toBeTruthy()
+  })
 
-test('elements check', () => {
-  const { container } = render(reactForm)
-  const loginInput = container.querySelector('#login')
-  const passwordInput = container.querySelector('#password')
-  expect(loginInput).toBeTruthy()
-  expect(passwordInput).toBeTruthy()
-})
+  test('false validation check', async () => {
+    inputs[0].value = '1'
+    inputs[1].value = '1'
+    const user = userEvent.setup()
+    render(reactForm)
+    const loginInput = screen.getByLabelText('Логин')
+    const passwordInput = screen.getByLabelText('Пароль')
+    await user.click(loginInput)
+    await user.click(passwordInput)
+    await user.click(loginInput)
+    expect(screen.getByText(REGEX_ERRORS.LOGIN)).toBeTruthy()
+    expect(screen.getByText(REGEX_ERRORS.PASSWORD)).toBeTruthy()
+  })
 
-test('false validation check', async () => {
-  inputs[0].value = '1'
-  inputs[1].value = '1'
-  const user = userEvent.setup()
-  render(reactForm)
-  const loginInput = screen.getByLabelText('Логин')
-  const passwordInput = screen.getByLabelText('Пароль')
-  await user.click(loginInput)
-  await user.click(passwordInput)
-  await user.click(loginInput)
-  expect(screen.getByText(REGEX_ERRORS.LOGIN)).toBeTruthy()
-  expect(screen.getByText(REGEX_ERRORS.PASSWORD)).toBeTruthy()
-})
-
-test('true validation check', async () => {
-  inputs[0].value = 'theo1'
-  inputs[1].value = 'Aaa101010'
-  const user = userEvent.setup()
-  render(reactForm)
-  const loginInput = screen.getByLabelText('Логин')
-  const passwordInput = screen.getByLabelText('Пароль')
-  await user.click(loginInput)
-  await user.click(passwordInput)
-  await user.click(loginInput)
-  expect(screen.queryByText(REGEX_ERRORS.LOGIN)).toBeFalsy()
-  expect(screen.queryByText(REGEX_ERRORS.PASSWORD)).toBeFalsy()
-})
+  test('true validation check', async () => {
+    inputs[0].value = 'theo1'
+    inputs[1].value = 'Aaa101010'
+    const user = userEvent.setup()
+    render(reactForm)
+    const loginInput = screen.getByLabelText('Логин')
+    const passwordInput = screen.getByLabelText('Пароль')
+    await user.click(loginInput)
+    await user.click(passwordInput)
+    await user.click(loginInput)
+    expect(screen.queryByText(REGEX_ERRORS.LOGIN)).toBeFalsy()
+    expect(screen.queryByText(REGEX_ERRORS.PASSWORD)).toBeFalsy()
+  })
 })
