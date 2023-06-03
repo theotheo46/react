665d18664099e4b2d381e4c73b9560e59049b0a4
an3wers <an3wer@yandex.ru>
2023-06-04 01:42:04 +0500

    format

--- packages/client/path2d-polyfill.ts
+++ packages/client/path2d-polyfill.ts
@@ -1,5 +1,4 @@
-import { Path2D } from "path2d-polyfill"
+import { Path2D } from 'path2d-polyfill'
 const Path2DPolyfill = typeof window !== 'undefined' ? window.Path2D : Path2D
-
 
 export default Path2DPolyfill
