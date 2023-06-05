bb2a359f6ed66bbc88817c60282454d254f5ab40
an3wers <an3wer@yandex.ru>
2023-06-06 00:34:18 +0500

    ssr new try

--- packages/client/path2d-polyfill.ts
+++ packages/client/path2d-polyfill.ts
@@ -1,4 +1,14 @@
-import { Path2D } from 'path2d-polyfill'
-const Path2DPolyfill = typeof window !== 'undefined' ? window.Path2D : Path2D
+const createPath2DPolyfill = async () => {
+  let Path2DPolyfill: Path2DConstructor | null = null
 
-export default Path2DPolyfill
+  if (typeof window == 'undefined') {
+    // import('path2d-polyfill').then(m => (Path2DPolyfill = m.default.Path2D))
+    Path2DPolyfill = (await import('path2d-polyfill')).default.Path2D
+  } else {
+    Path2DPolyfill = window.Path2D
+  }
+
+  return Path2DPolyfill
+}
+
+export default createPath2DPolyfill
--- packages/client/src/components/Bottle/TypeContourBottle.tsx
+++ packages/client/src/components/Bottle/TypeContourBottle.tsx
@@ -1,15 +1,23 @@
-import Path2D from '../../../path2d-polyfill'
+import Path2DPolyfill from '../../../path2d-polyfill'
 
-class TypeContourBottle {
-  id: number
-  pathStr: string
-  path: Path2D
+// console.log('Path2DPolyfill', Path2DPolyfill, typeof window)
 
-  constructor(id: number, pathStr: string) {
-    this.id = id
-    this.pathStr = pathStr
-    this.path = new Path2D(pathStr)
+export default async () => {
+  const P = await Path2DPolyfill()
+
+  class TypeContourBottle {
+    id: number
+    pathStr: string
+    path: Path2D
+
+    constructor(id: number, pathStr: string) {
+      this.id = id
+      this.pathStr = pathStr
+      // this.path = new Path2D(pathStr)
+      this.path = new P(pathStr)
+    }
   }
+  return TypeContourBottle
 }
 
-export default TypeContourBottle
+// export default TypeContourBottle
