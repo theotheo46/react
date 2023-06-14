a9287cba1d580af21124c6ba2a7d0196718b42c2
Dmitry <theotheo46@gmail.com>
2023-06-14 19:39:31 +0300

    окончена разработка

--- packages/server/src/models/Leaderboard.ts
+++ packages/server/src/models/Leaderboard.ts
@@ -2,7 +2,6 @@ import { Model, Column, Table, DataType } from 'sequelize-typescript'
 
 @Table
 export class Leaderboard extends Model<Leaderboard> {
-
   @Column({ type: DataType.INTEGER })
   userId!: number
 
--- packages/server/src/routes/leaderboards.ts
+++ packages/server/src/routes/leaderboards.ts
@@ -3,38 +3,37 @@ import { Leaderboard } from '../models/Leaderboard'
 
 export const lb = Router()
 
-const getScore = (level : number, steps: number, time: number) : number => {
-  return Math.round(level / (time + steps * 5) * 100000)
+const getScore = (level: number, steps: number, time: number): number => {
+  return Math.round((level / (time + steps * 5)) * 100000)
 }
 
 lb.post('/setleader', async (req, res) => {
-    req.body.score = getScore(+req.body.level, +req.body.steps, +req.body.time)
-    console.log(req.body)
-    Leaderboard.create(req.body)
-    .then((leader)=>{
+  req.body.score = getScore(+req.body.level, +req.body.steps, +req.body.time)
+  console.log(req.body)
+  Leaderboard.create(req.body)
+    .then(leader => {
       return res.status(201).json(leader)
     })
     .catch(err => {
-      return res.status(400).json({err})
+      return res.status(400).json({ err })
     })
 })
 
-
 lb.get('/gettopleaders', async (req, res) => {
-    let number = req.query.number;
-    if (number===undefined) number = "10"
-    Leaderboard.findAll({
-      order: [
-        ["score", "DESC"],
-        ["usernick", "ASC"],
-      ],
-      limit: +number!
-    })
-    .then((leaders)=>{
+  let number = req.query.number
+  if (number === undefined) number = '10'
+  Leaderboard.findAll({
+    order: [
+      ['score', 'DESC'],
+      ['usernick', 'ASC'],
+    ],
+    limit: +number!,
+  })
+    .then(leaders => {
       return res.status(201).json(leaders)
     })
     .catch(err => {
-      return res.status(400).json({err})
+      return res.status(400).json({ err })
     })
 })
 
