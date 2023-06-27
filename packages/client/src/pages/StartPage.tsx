29dd6f0bd4f67a0dc0b386b6aea5a52d2055257f
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2023-06-27 22:43:10 +0500

    Изменил дизайн страницы настроек уровня

--- packages/client/src/pages/StartPage.tsx
+++ packages/client/src/pages/StartPage.tsx
@@ -1,9 +1,11 @@
 import StartGame from '../components/Game/StartGame'
+import { useAppSelector } from '../store/hooks'
 
 const StartPage = () => {
+  const { isSetupLevelSettings } = useAppSelector(state => state.game)
   return (
     <div className="page-wrap page-wrap_lightblue">
-      <main className="container">
+      <main className={`container ${isSetupLevelSettings && 'no-background'}`}>
         <StartGame />
       </main>
     </div>
