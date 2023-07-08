3c17a619f50186c256fbb2a3ca3e80bc67b77359
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2023-07-08 12:58:58 +0500

    delete comments

--- packages/server/src/routes/auth.ts
+++ packages/server/src/routes/auth.ts
@@ -3,21 +3,7 @@ import { YandexApi } from '../api/yandexApi'
 
 export const auth = Router()
 
-// auth.post('/setleader', async (req, res) => {
-//   //BODY {"userId":"12345","usernick":"theo","level":"15","steps":"49","time":"120","score":""}
-//   req.body.score = getScore(+req.body.level, +req.body.steps, +req.body.time)
-//   console.log(req.body)
-//   Leaderboard.create(req.body)
-//     .then(leader => {
-//       return res.status(201).json(leader)
-//     })
-//     .catch(err => {
-//       return res.status(400).json({ err })
-//     })
-// })
-
 auth.get('/user', async (req, res) => {
-  //
   console.log('cookie form auth:', req.headers['cookie'])
 
   try {
