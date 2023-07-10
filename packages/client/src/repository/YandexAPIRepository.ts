a29e26d2dbd6ace457f93ca099d90d69d636bf18
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2023-07-10 05:09:26 +0500

    modified:   packages/client/src/repository/YandexAPIRepository.ts

--- packages/client/src/repository/YandexAPIRepository.ts
+++ packages/client/src/repository/YandexAPIRepository.ts
@@ -4,12 +4,9 @@ import { User } from '../store/slices/userSlice/types'
 
 export class YandexAPIRepository implements UserRepository {
   async getCurrent(): Promise<User> {
-    const { data } = await axios.get(
-      `http://localhost:${__SERVER_PORT__}/api/v2/auth/user`,
-      {
-        withCredentials: true,
-      }
-    )
+    const { data } = await axios.get(`/api/v2/auth/user`, {
+      withCredentials: true,
+    })
     return data
   }
 }
